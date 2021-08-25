dbab261dc54eb106115348b9be201efb2cb66a28
HiraArain <hiraarain100@gmail.com>
2021-08-25 00:37:16 -0700

    chat app files

---
+++ chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Room(models.Model):
+    name =models.CharField(max_length=50)
+
+
+
+class Message(models.Model):
+    value =models.CharField(max_length=200)
+    date = models.DateTimeField(default=datetime.now,blank = True)
+    user = models.CharField(max_length=5000)
+    room = models.CharField(max_length=5000)
